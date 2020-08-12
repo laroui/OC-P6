5baa9875a198297bed446b552c3615ebf3014055
LAROUI Nacim <l.nassimo@gmail.com>
2020-08-12 14:01:27 +0200

    Sauce.js Schema

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -3,12 +3,14 @@ const sauceSchema =mongoose.Schema({
     userId:{type: String , required},
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
-    name: { type: String, required: true },
-    name: { type: String, required: true },
     description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
     imageUrl: { type: String, required: true },
-    userId: { type: String, required: true },
-    price: { type: Number, required: true },
+    usersLiked: { type: [String], required: true },
+    usersDisliked: { type: [String], required: true },
 });
 
 module.exports = mongoose.model('sauce', sauceSchema);
