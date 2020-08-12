e2685c14703f9ca592887009e99a827158228395
LAROUI Nacim <l.nassimo@gmail.com>
2020-08-12 14:44:18 +0200

    package.json completion linking Mutler to sauce routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,30 @@ const express = require ('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const user = require('./models/User');
+const Thing = require('./models/thing');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 
 
+
+
+mongoose.connect('mongodb+srv://dzaltern:passfor@cluster0-kqhbz.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(bodyParser.json());
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth',userRoutes)
+
+module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const sauce = require('../controllers/sauce');
+
+//Import du middleware auth pour sécuriser les routes
+const auth = require('../middleware/auth');
+//Import du middleware multer pour la gestion des images
+const multer = require('../middleware/mt-conf');
+
+//router.post('/', auth, sauce.ANALYSE);
+router.put('/:id', auth, multer, sauce.update)
+router.delete('/:id', auth, sauce.delete);
+
+router.post('/', auth, multer, sauce.create);
+router.get('/', auth, sauce.list);
+router.get('/:id', auth, sauce.OneSauce);
+router.post('/:id/like', auth, sauce.likeSauce);
+
+module.exports = router;
